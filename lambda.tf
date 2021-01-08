5080561d069fa9ccacbaf7b4b3b68abd2d3b4bf1
domingobishop <domingobishop@gmail.com>
2021-01-08 10:25:53 +0000

    rename function

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "rds_out_of_hours_shutdown" {
     filename         = "lambda-function.zip"
-    function_name    = "rds_out_of_hours_shutdown"
+    function_name    = "rds-out-of-hours-shutdown"
     role             = aws_iam_role.rds_out_of_hours_shutdown.arn
     handler          = "rds_out_of_hours_shutdown.lambda_handler"
     source_code_hash = data.archive_file.lambda_zip.output_base64sha256
